ff28d757aa37416aba50c960afac43bbd1045684
rogerpujolnavarro <rogerpujolnavarro@gmail.com>
2022-02-17 10:42:57 +0100

    Canvis estils, max-height variable pendent

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,5 +1,5 @@
 // react
-import { useContext } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { FiCornerDownLeft, FiRefreshCcw } from 'react-icons/fi';
 // components
 import Pins from './Pins';
@@ -11,9 +11,55 @@ import PinContext from '../contexts/PinContext';
 import SolutionContext from '../contexts/SolutionContext';
 
 const Board = () => {
+	const [boardHeight, setBoardHeight] = useState(0);
+	const [showBoard, setShowBoard] = useState(false);
+
 	const { attempt, onChangeAttempt, totalAttempts, totalColumns } = useContext(AttemptContext);
 	const { targetHoles } = useContext(PinContext);
 	const { endGame, onNewGame } = useContext(SolutionContext);
+
+	const boardRef = useRef();
+	const mainRef = useRef();
+	const solutionRef = useRef();
+	const pinBoardRef = useRef();
+
+	useEffect(() => {
+		boardRef.current.scrollIntoView({ behavior: 'smooth' });
+	}, [attempt, boardRef]);
+
+	const resizeBoard = () => {
+		setShowBoard(false);
+		const newHeight =
+			mainRef.current.clientHeight -
+			solutionRef.current.offsetHeight -
+			pinBoardRef.current.offsetHeight -
+			80;
+		setBoardHeight(newHeight);
+	};
+
+	useEffect(() => {
+		window.addEventListener('resize', resizeBoard);
+		setShowBoard(false);
+		resizeBoard();
+	}, []);
+
+	// useEffect(() => {
+	// 	console.log('main: ' + mainRef.current.clientHeight);
+	// 	console.log('sol: ' + solutionRef.current.offsetHeight);
+	// 	console.log('pin: ' + pinBoardRef.current.offsetHeight);
+
+	// 	setBoardHeight(
+	// 		mainRef.current.clientHeight -
+	// 			solutionRef.current.offsetHeight -
+	// 			pinBoardRef.current.offsetHeight -
+	// 			80
+	// 	);
+	// }, [mainRef, pinBoardRef, solutionRef]);
+
+	useEffect(() => {
+		console.log(boardHeight);
+		setShowBoard(true);
+	}, [boardHeight]);
 
 	let board = [];
 	for (let currentRow = 0; currentRow < totalAttempts; currentRow++) {
@@ -25,43 +71,47 @@ const Board = () => {
 	}
 
 	return (
-		<main>
-			<div className="solution">
-				<div>
-					<ul>
-						{board[0].map((id) => (
-							<Solution key={`solution-${id}`} id={id} />
-						))}
-					</ul>
-					<button>
-						<FiRefreshCcw onClick={onNewGame} />
-					</button>
-				</div>
+		<main ref={mainRef}>
+			<div className="solution" ref={solutionRef}>
+				<ul>
+					{board[0].map((id) => (
+						<Solution key={`solution-${id}`} id={id} />
+					))}
+				</ul>
+				<button>
+					<FiRefreshCcw onClick={onNewGame} />
+				</button>
 			</div>
+			<div>
+				<div className="board" style={{ maxHeight: boardHeight }}>
+					{showBoard &&
+						board.map((row, index) => (
+							<TableRow key={`table-${index}`} row={row} indexRow={index} />
+						))}
+					<div ref={boardRef}></div>
+				</div>
 
-			{board.map((row, index) => (
-				<TableRow key={`table-${index}`} row={row} indexRow={index} />
-			))}
-			<div className="pin-board">
-				{!endGame && (
-					<>
-						<span className="attempts">
-							{totalAttempts - attempt + 1 + '⌁' + totalAttempts}
-						</span>
-						<Pins />
-						<button
-							className={`${
-								targetHoles.length ===
-									(totalAttempts - attempt + 1) * totalColumns && !endGame
-									? ''
-									: 'disabled'
-							}`}
-							onClick={onChangeAttempt}
-						>
-							<FiCornerDownLeft />
-						</button>
-					</>
-				)}
+				<div className="pin-board" ref={pinBoardRef}>
+					{!endGame && (
+						<>
+							<button className="attempts" disabled>
+								{totalAttempts - attempt + 1 + '⌁' + totalAttempts}
+							</button>
+							<Pins />
+							<button
+								className={`${
+									targetHoles.length ===
+										(totalAttempts - attempt + 1) * totalColumns && !endGame
+										? ''
+										: 'disabled'
+								}`}
+								onClick={onChangeAttempt}
+							>
+								<FiCornerDownLeft />
+							</button>
+						</>
+					)}
+				</div>
 			</div>
 		</main>
 	);
