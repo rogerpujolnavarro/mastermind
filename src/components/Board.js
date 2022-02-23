f0b2f233f9e7c89a80a82b1073c6444df2ee8e3e
rogerpujolnavarro <rogerpujolnavarro@gmail.com>
2022-02-23 17:40:46 +0100

    Estils div board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,5 +1,5 @@
 // react
-import { useContext, useEffect, useRef, useState } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 import { FiCornerDownLeft, FiRefreshCcw } from 'react-icons/fi';
 // components
 import Pins from './Pins';
@@ -11,55 +11,15 @@ import PinContext from '../contexts/PinContext';
 import SolutionContext from '../contexts/SolutionContext';
 
 const Board = () => {
-	const [boardHeight, setBoardHeight] = useState(0);
-	const [showBoard, setShowBoard] = useState(false);
-
 	const { attempt, onChangeAttempt, totalAttempts, totalColumns } = useContext(AttemptContext);
 	const { targetHoles } = useContext(PinContext);
 	const { endGame, onNewGame } = useContext(SolutionContext);
 
 	const boardRef = useRef();
-	const mainRef = useRef();
-	const solutionRef = useRef();
-	const pinBoardRef = useRef();
 
 	useEffect(() => {
 		boardRef.current.scrollIntoView({ behavior: 'smooth' });
 	}, [attempt, boardRef]);
-
-	const resizeBoard = () => {
-		setShowBoard(false);
-		const newHeight =
-			mainRef.current.clientHeight -
-			solutionRef.current.offsetHeight -
-			pinBoardRef.current.offsetHeight -
-			80;
-		setBoardHeight(newHeight);
-	};
-
-	useEffect(() => {
-		window.addEventListener('resize', resizeBoard);
-		setShowBoard(false);
-		resizeBoard();
-	}, []);
-
-	// useEffect(() => {
-	// 	console.log('main: ' + mainRef.current.clientHeight);
-	// 	console.log('sol: ' + solutionRef.current.offsetHeight);
-	// 	console.log('pin: ' + pinBoardRef.current.offsetHeight);
-
-	// 	setBoardHeight(
-	// 		mainRef.current.clientHeight -
-	// 			solutionRef.current.offsetHeight -
-	// 			pinBoardRef.current.offsetHeight -
-	// 			80
-	// 	);
-	// }, [mainRef, pinBoardRef, solutionRef]);
-
-	useEffect(() => {
-		console.log(boardHeight);
-		setShowBoard(true);
-	}, [boardHeight]);
 
 	let board = [];
 	for (let currentRow = 0; currentRow < totalAttempts; currentRow++) {
@@ -71,8 +31,8 @@ const Board = () => {
 	}
 
 	return (
-		<main ref={mainRef}>
-			<div className="solution" ref={solutionRef}>
+		<main>
+			<div className="solution">
 				<ul>
 					{board[0].map((id) => (
 						<Solution key={`solution-${id}`} id={id} />
@@ -83,15 +43,13 @@ const Board = () => {
 				</button>
 			</div>
 			<div>
-				<div className="board" style={{ maxHeight: boardHeight }}>
-					{showBoard &&
-						board.map((row, index) => (
-							<TableRow key={`table-${index}`} row={row} indexRow={index} />
-						))}
+				<div className="board">
+					{board.map((row, index) => (
+						<TableRow key={`table-${index}`} row={row} indexRow={index} />
+					))}
 					<div ref={boardRef}></div>
 				</div>
-
-				<div className="pin-board" ref={pinBoardRef}>
+				<div className="pin-board">
 					{!endGame && (
 						<>
 							<button className="attempts" disabled>
